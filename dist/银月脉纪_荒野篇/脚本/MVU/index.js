d2367b0c6285437e9eb3cbe367458c480fe35830
BaiMeow <3652290768@qq.com>
2026-06-20 13:56:21 +0800

    切换production构建模式:dist从2.9MB降到211KB,修复酒馆iframe内instanceof错误

--- dist/银月脉纪_荒野篇/脚本/MVU/index.js
+++ dist/银月脉纪_荒野篇/脚本/MVU/index.js
@@ -1,11 +1,1 @@
-import "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@c1ae3a921bdab2e33b1afc745e5474b9b07fc070/artifact/bundle.js";
-
-var __webpack_modules__ = {
-  389() {
-    eval('{\n;// external "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@c1ae3a921bdab2e33b1afc745e5474b9b07fc070/artifact/bundle.js"\n\n;// ./src/银月脉纪_荒野篇/脚本/MVU/index.ts\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMzg5LmpzIiwibWFwcGluZ3MiOiI7Ozs7QUNBbUkiLCJzb3VyY2VzIjpbInNyYzovL3RhdmVybl9oZWxwZXJfdGVtcGxhdGUvZXh0ZXJuYWwgbW9kdWxlIFwiaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZUBjMWFlM2E5MjFiZGFiMmUzM2IxYWZjNzQ1ZTU0NzRiOWIwN2ZjMDcwL2FydGlmYWN0L2J1bmRsZS5qc1wiIiwic3JjOi8vdGF2ZXJuX2hlbHBlcl90ZW1wbGF0ZS9zcmMv6ZO25pyI6ISJ57qqX+iNkumHjuevhy/ohJrmnKwvTVZVL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbIiIsImltcG9ydCAnaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZUBjMWFlM2E5MjFiZGFiMmUzM2IxYWZjNzQ1ZTU0NzRiOWIwN2ZjMDcwL2FydGlmYWN0L2J1bmRsZS5qcyc7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///389\n\n}');
-  }
-};
-
-var __webpack_exports__ = {};
-
-__webpack_modules__[389]();
+import'https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@c1ae3a921bdab2e33b1afc745e5474b9b07fc070/artifact/bundle.js';
